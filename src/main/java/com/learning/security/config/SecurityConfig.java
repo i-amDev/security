fb551d5f4127db4feb408ae618c85fd4de2b2eb2
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-01-05 12:59:22 +0530

    - Created multiple users using "InMemoryUserDetailsManager". - Commented the previously created users from the application.properties.

--- src/main/java/com/learning/security/config/SecurityConfig.java
+++ src/main/java/com/learning/security/config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.net.http.HttpRequest;
@@ -19,4 +23,22 @@ public class SecurityConfig {
         httpSecurity.httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}password1")
+                .build();
+
+        UserDetails user2 = User.withUsername("user2")
+                .password("{noop}password2")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}admin")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2, admin);
+    }
+
 }
