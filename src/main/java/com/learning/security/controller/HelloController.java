ac9b660b8bb3dc37de7133b376f3736c6e5bd728
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-01-06 14:35:52 +0530

    - Autowired the "AuthenticationManager" and "JwtUtils" beans. - Added a "POST" mapping for "signin" method. - Implemented the logic to return the JWT token generated from username.

--- src/main/java/com/learning/security/controller/HelloController.java
+++ src/main/java/com/learning/security/controller/HelloController.java
@@ -1,11 +1,28 @@
 package com.learning.security.controller;
 
+import com.learning.security.dto.LoginRequest;
+import com.learning.security.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
 
     @GetMapping("/hello")
 //    @PreAuthorize("hasRole('ADMIN')")
@@ -23,4 +40,26 @@ public class HelloController {
     public String sayUserHello() {
         return "Hello, User.";
     }
+
+    @PostMapping("/signin")
+    public String login(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication;
+        try{
+            authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        }
+        catch (AuthenticationException e) {
+            e.printStackTrace();
+            return "Could not authenticate";
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        String jwtToken = jwtUtils.generateTokenFromUsername(userDetails.getUsername());
+
+        return jwtToken;
+    }
+
+
 }
