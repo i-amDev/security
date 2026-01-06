dcf17822d99db22c98d949d8531c718d9c41468d
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-01-06 14:39:53 +0530

    - Refactored. - Removed unused imports.

--- src/main/java/com/learning/security/config/SecurityConfig.java
+++ src/main/java/com/learning/security/config/SecurityConfig.java
@@ -14,12 +14,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
-import java.net.http.HttpRequest;
 
 @Configuration
 @EnableWebSecurity
